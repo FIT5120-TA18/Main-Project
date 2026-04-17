1925d45e40db05d9536fe15860ff6b0ca9cbc7e5
Akshat <adaw0014@student.monash.edu>
2026-04-17 15:20:47 +1000

    Fix itration 1 build

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,11 @@
 # Flask backend, routing the pages
 from flask import Flask
+import os
 
 def create_app():
     app = Flask(__name__)
 
-    app.config["SECRET_KEY"] = sec_key
+    app.config["SECRET_KEY"] = os.getenv('sec_key')
 
     from app.routes import main
     app.register_blueprint(main)
